269ca4b33af536235dc11704fe8b6bcb5e696eeb
Pooja Modi <pooja18modi@gmail.com>
2022-08-17 12:58:42 +0530

    practice

--- src/Assignment/Literals.java
+++ src/Assignment/Literals.java
@@ -3,6 +3,13 @@ package Assignment;
 public class Literals {
     public static void main(String[] args) {
         int i = 1_000_000;
-        System.out.println(i);
+//        System.out.println(i);
+        StringBuilder s = new StringBuilder("1Z0");
+        System.out.println(s.capacity());
+        s.append("-808");
+        System.out.println(s);
+        System.out.println(s.length());
+        System.out.println(s.capacity());
+
     }
 }
